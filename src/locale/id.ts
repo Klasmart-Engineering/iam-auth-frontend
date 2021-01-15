45b03311f36169022e7394735ee8a9c49f2db9ab
Shawn Lee <shawn.lee@calmid.com>
2021-01-15 20:11:44 +0900

    Added indonesian

---
+++ src/locale/id.ts
@@ -0,0 +1,46 @@
+// id.ts
+const messages: Record<string, string> = {
+    button_continue: "Lanjut",
+
+    component_switchDark: "gelap",
+    component_switchLight: "terang",
+
+    continue_signInSuccess: "Berhasil Masuk",
+    continue_continuePrompt: "Membawa Anda secara otomatis ke <em> {continueLink} </em>!",
+    continue_countdownToContinue: "Jika tidak ada yang terjadi dalam {seconds} detik, klik \"Lanjut\".",
+
+    error_emptyEmail: "Masukkan email atau nomor telepon",
+    error_emptyPassword: "Masukkan kata sandi",
+    error_emptyVerificationCode: "Harap masukkan kode verifikasi dari {device} Anda.",
+    error_invalidRedirectLink: "Sepertinya <em> {continueLink} </em> bukan bagian dari KidsLoop! Demi keamanan Anda, kami akan membawa Anda ke <em> {defaultLink} </em>.",
+
+    form_emailLabel: "Email atau Telepon",
+    form_passwordLabel: "Kata Sandi",
+    form_verificationLabel: "Kode Verifikasi",
+
+    locale_select: "Pilih Bahasa",
+    locale_tooltip: "Ganti Bahasa",
+
+    login_acceptPrivacyPolicy: "Saya menyetujui Kebijakan Privasi KidsLoop",
+    login_createAccount: "Buat sebuah akun",
+    login_forgotPassword: "Tidak Ingat Kata Sandi?",
+    login_loginButton: "Masuk",
+    login_loginPrompt: "Masuk",
+
+    notFound_notFoundPrompt: "Ups! Tautan rusak.",
+    notFound_notFoundDescription: "Langsung ke beranda.",
+    notFound_homeButton: "Beranda",
+
+    signup_signIn: "Masuk Saja",
+    signup_signUpNextButton: "Lanjut",
+    signup_signUpPrompt: "Daftar",
+
+    verify_backButton: "Sebelum",
+    verify_verifyButton: "Kirimkan",
+    verify_verifyPrompt: "Konfirmasi Akun",
+
+    privacy_helpLink: "Bantuan",
+    privacy_privacyLink: "Pribadi",
+    privacy_termsLink: "Syarat",
+};
+export default messages;
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -27,6 +27,10 @@ const LANGUAGES_LABEL: Language[] = [
         code: "vi",
         text: "Tiếng Việt",
     },
+    {
+        code: "id",
+        text: "bahasa Indonesia",
+    }
 ];
 
 const useStyles = makeStyles((theme) => createStyles({
