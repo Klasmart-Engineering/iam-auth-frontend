6684c1fda3f9ecb6b026ecb490286aa40692911a
Guy Richardson <guy.richardson@opencredo.com>
2021-04-23 13:09:54 +0000

    Merged in KI-171-BitbucketPipelineBuild (pull request #6)
    KI-171 BitbucketPipelineBuild
    
    * Aligned naming of webpack.prod with other webpack configs
    
    * Added init work at deployment through Bitbucket pipelines
    
    * Syntax error in mv
    
    * update image to python 3.9-alpine
    
    * Realigned image tagging for Bitbucket Pipelines and added tentative config for VN and ID
    
    * Missed merge - accepted change
    
    * Refactored pipeline to use DOCKER_REPO_URL workspace variable and new login
    
    * Pipeline: typo, wrong login domain for ECR
    
    * Pipelines: typo changed from hub to auth
    
    * Made branch tag lowercase in bitbucket pipeline
    
    Approved-by: Owen Delahoy
    Approved-by: Sebastian Margineanu

---
+++ deploy/config/in/webpack.prod.config.js
@@ -0,0 +1,101 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+    mode: "production",
+    entry: {
+        ui: "./src/entry.tsx",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    "css-modules-typescript-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                        }
+                    },
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    "file-loader",
+                ],
+            },
+            {
+                test: /\.mp4$/,
+                use: "file-loader?name=videos/[name].[ext]",
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".tsx", ".ts"],
+        alias: {
+            react: path.resolve("./node_modules/react"),
+        },
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html"
+        }),
+        new webpack.EnvironmentPlugin({
+            "API_ENDPOINT": "https://api.kidsloop.in/",
+            "AUTH_ENDPOINT": "https://auth.kidsloop.in/",
+            "REDIRECT_LINK": "https://hub.kidsloop.in/",
+            "ACCOUNT_ENDPOINT_BADANAMU": "https://ams-account.badanamu.net",
+            "AUTH_ENDPOINT_BADANAMU": "https://ams-auth.prod.badanamu.net",
+            "SLD": "kidsloop",
+            "TLD": "in",
+        })
+    ],
+    devServer: {
+        host: "0.0.0.0",
+        disableHostCheck: true,
+        historyApiFallback: true,
+        proxy: {
+            "/transfer": {
+                target: "http://localhost:8081",
+                secure: false,
+            },
+
+            "/refresh": {
+                target: "http://localhost:8081",
+                secure: false,
+            },
+        }
+    },
+};
