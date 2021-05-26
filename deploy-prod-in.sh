4e31f24434f25e16de85aabd56f45f1298ceb74d
jeangougou <jeangougou@gmail.com>
2021-05-26 13:58:18 +0100

    add chmod +x on new build script, add config in package json

--- deploy-prod-in.sh
+++ deploy-prod-in.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+
+# submodules
 git submodule update --init --recursive --force
+# npm installs
 npm i --no-progress
 npm audit fix
+
+# config hack and package build
 API_ENDPOINT="https://api-prod.kidsloop.in/" \
 AUTH_ENDPOINT="https://auth-prod.alpha.kidsloop.in/" \
 REDIRECT_LINK="https://hub-prod.alpha.kidsloop.in/" \
