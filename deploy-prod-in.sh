bf726bfd77fa7f5290b4a389ebac8b3a560a4894
owen-delahoy <owen.delahoy@calmid.com>
2021-05-27 16:44:50 +0900

    India deploy script  changes

--- deploy-prod-in.sh
+++ deploy-prod-in.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
-
-# submodules
-git submodule update --init --recursive --force
-# npm installs
-npm i --no-progress
+npm ci --no-progress
 npm audit fix
-
-# config hack and package build
-API_ENDPOINT="https://api-prod.kidsloop.in/" \
-AUTH_ENDPOINT="https://auth-prod.alpha.kidsloop.in/" \
-REDIRECT_LINK="https://hub-prod.alpha.kidsloop.in/" \
+API_ENDPOINT="https://api.kidsloop.in/" \
+AUTH_ENDPOINT="https://auth.kidsloop.in/" \
+REDIRECT_LINK="https://hub.kidsloop.in/" \
+ACCOUNT_ENDPOINT_BADANAMU="https://ams-account.badanamu.net" \
+AUTH_ENDPOINT_BADANAMU="https://ams-auth.badanamu.net" \
 SLD="kidsloop" \
 TLD="in" \
-npm run build:prod-in
-
-# s3 bucket sync
-aws s3 sync dist s3://klindia-prod-auth/latest
-
-# invalidate cache
+npm run build:prod
+echo "Waiting 60s before deploying to production in india"
+sleep 60
+aws s3 sync dist s3://klindia-prod-auth/latest --delete
 aws cloudfront create-invalidation --paths "/*" --distribution-id E2DJ7W3VTT0CDT
