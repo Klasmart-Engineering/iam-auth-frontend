0b2db08f93a8f9e99887df705139cb99f51a2142
Guy Richardson <guy@Opencredos-MacBook-Pro.local>
2021-05-27 10:01:38 +0100

    Remove re-added config files

--- deploy-prod-vn.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-npm i
-npm run build:prod-vn
-aws s3 sync dist s3://kidsloop-auth-prod/.vn/ --region ap-northeast-2 --dryrun
-echo "Waiting 60s before deploying to production in vietnam"
-sleep 60
-aws s3 sync dist s3://kidsloop-auth-prod/.vn/ --region ap-northeast-2
-aws cloudfront create-invalidation --paths "/*" --distribution-id E5H24J1OSZPKD --region ap-northeast-2 #auth.kidsloop.vn
