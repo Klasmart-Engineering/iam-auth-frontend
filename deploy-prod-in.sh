4211dd6b7849a8395313d449a4d762a57d9e0145
Jean Georges <jean.georges@calmid.com>
2021-06-01 07:37:32 +0100

    finalize single target deployment for india

--- deploy-prod-in.sh
+++ deploy-prod-in.sh
@@ -7,12 +7,12 @@ npm i --no-progress
 npm audit fix
 
 # config hack and package build
-API_ENDPOINT="https://api-prod.kidsloop.in/" \
-AUTH_ENDPOINT="https://auth-prod.alpha.kidsloop.in/" \
-REDIRECT_LINK="https://hub-prod.alpha.kidsloop.in/" \
+API_ENDPOINT="https://api.kidsloop.in/" \
+AUTH_ENDPOINT="https://auth.alpha.kidsloop.in/" \
+REDIRECT_LINK="https://hub.alpha.kidsloop.in/" \
 SLD="kidsloop" \
 TLD="in" \
-npm run build:prod-in
+npm run build:prod
 
 # s3 bucket sync
 aws s3 sync dist s3://klindia-prod-auth/latest
