7f534478997bb44cffc2b3b2e540e7d13708eae0
Owen Delahoy <owen.delahoy@gmail.com>
2021-06-09 22:18:45 +0900

    Fix staging script

--- deploy-stage.sh
+++ deploy-stage.sh
@@ -9,7 +9,7 @@ ACCOUNT_ENDPOINT_BADANAMU="https://ams-account.badanamu.net" \
 AUTH_ENDPOINT_BADANAMU="https://ams-auth.badanamu.net" \
 SLD="kidsloop" \
 TLD="net" \
-npm run build:dev
+npm run build:prod
 
 aws s3 sync dist s3://auth.kidsloop.net/stage --dryrun
 sleep 5
