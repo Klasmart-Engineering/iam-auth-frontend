26a3fe1c810d912b11e013479bcc2dbe1df66152
Stefan Andrei <stefan.andrei@opencredo.com>
2021-06-21 15:13:44 +0000

    Merged in OPSBAU-49/create_rk_webpack_config (pull request #38)
    add rumahkisah config files
    
    * add rumahkisah config files
    
    * build in prod mode
    
    Approved-by: Guy Richardson

---
+++ deploy/config/id/webpack.rk_beta.config.js
@@ -0,0 +1,109 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+    mode: "production",
+    entry: {
+        ui: "./src/entry.tsx",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    "css-modules-typescript-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            // mozjpeg: {
+                            //     progressive: true,
+                            //     quality: 65
+                            // },
+                            // // optipng.enabled: false will disable optipng
+                            // optipng: {
+                            //     enabled: false,
+                            // },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                        }
+                    },
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    "file-loader",
+                ],
+            },
+            {
+                test: /\.mp4$/,
+                use: "file-loader?name=videos/[name].[ext]",
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".tsx", ".ts"],
+        alias: {
+            react: path.resolve("./node_modules/react"),
+        },
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html"
+        }),
+        new webpack.EnvironmentPlugin({
+            "API_ENDPOINT": "https://api.beta.rumahkisah.net/",
+            "AUTH_ENDPOINT": "https://auth.beta.rumahkisah.net/",
+            "REDIRECT_LINK": "https://hub.rumahkisah.net/",
+            "ACCOUNT_ENDPOINT_BADANAMU": "https://ams-account.badanamu.net",
+            "AUTH_ENDPOINT_BADANAMU": "https://prod.auth.badanamu.net",
+            "SLD": "rumahkisah",
+            "TLD": "net",
+        })
+    ],
+    devServer: {
+        host: "0.0.0.0",
+        disableHostCheck: true,
+        historyApiFallback: true,
+        proxy: {
+            "/transfer": {
+                target: "http://localhost:8081",
+                secure: false,
+            },
+
+            "/refresh": {
+                target: "http://localhost:8081",
+                secure: false,
+            },
+        }
+    },
+};
