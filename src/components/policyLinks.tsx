06a77769acf3adf380aba4f99298d47197a0085a
Max Nicholson <max.nicholson@calmid.com>
2021-06-21 14:09:38 +0100

    feat: use PrivacyPolicy from kidsloop-branding

--- src/components/policyLinks.tsx
+++ src/components/policyLinks.tsx
@@ -1,17 +1,22 @@
-import useMediaQuery from "@material-ui/core/useMediaQuery"
-import DialogContent from "@material-ui/core/DialogContent"
-import DialogContentText from "@material-ui/core/DialogContentText"
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
 import Dialog from "@material-ui/core/Dialog";
 import Grid from "@material-ui/core/Grid";
 import Grow from "@material-ui/core/Grow";
 import Link from "@material-ui/core/Link";
-import { createStyles, makeStyles, Theme, useTheme } from "@material-ui/core/styles";
+import {
+    createStyles,
+    makeStyles,
+    Theme,
+    useTheme,
+} from "@material-ui/core/styles";
 import { TransitionProps } from "@material-ui/core/transitions";
 import * as React from "react";
 import { useEffect, useRef, useState } from "react";
 import { FormattedMessage } from "react-intl";
-import PrivacyNotice from "../pages/policies/privacy";
 import DialogAppBar from "./dialogAppBar";
+import { PrivacyPolicy } from "@branding";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -22,18 +27,31 @@ const useStyles = makeStyles((theme: Theme) =>
                 textAlign: "center",
             },
         },
-    }),
+    })
 );
+
+function LinkContainer({ children }: { children: React.ReactNode }) {
+    const classes = useStyles();
+    return (
+        <Grid
+            container
+            spacing={2}
+            justify="flex-end"
+            className={classes.links}
+        >
+            {children}
+        </Grid>
+    );
+}
 
 const Motion = React.forwardRef(function Transition(
     props: TransitionProps & { children?: React.ReactElement },
-    ref: React.Ref<unknown>,
+    ref: React.Ref<unknown>
 ) {
     return <Grow style={{ transformOrigin: "0 0 0" }} ref={ref} {...props} />;
 });
 
 export default function PolicyLinks() {
-    const classes = useStyles();
     const theme = useTheme();
 
     const [open, setOpen] = useState(false);
@@ -52,45 +70,35 @@ export default function PolicyLinks() {
         }
     }, [open]);
 
+    if (PrivacyPolicy.variant === "link") {
+        return (
+            <LinkContainer>
+                <Grid item xs={4}>
+                    <PrivacyPolicy color="inherit" variant="caption">
+                        <FormattedMessage id="privacy_privacyLink" />
+                    </PrivacyPolicy>
+                </Grid>
+            </LinkContainer>
+        );
+    }
+
     return (
         <>
-            <Grid container spacing={2} justify="flex-end" className={classes.links}>
-                {/* <Grid item xs={4}>
-                    <Link
-                        color="inherit"
-                        variant="caption"
-                        onClick={() => { 
-                            setOpen(true);
-                        }}
-                    >
-                        <FormattedMessage id="privacy_helpLink" />
-                    </Link>
-                </Grid> */}
+            <LinkContainer>
                 <Grid item xs={4}>
                     <Link
                         color="inherit"
                         variant="caption"
-                        onClick={() => { 
+                        onClick={() => {
                             setOpen(true);
-                            setTitle("Privacy Notice")
+                            setTitle("Privacy Notice");
                         }}
                         style={{ cursor: "pointer" }}
                     >
                         <FormattedMessage id="privacy_privacyLink" />
                     </Link>
                 </Grid>
-                {/* <Grid item xs={4}>
-                    <Link
-                        color="inherit"
-                        variant="caption"
-                        onClick={() => { 
-                            setOpen(true);
-                        }}
-                    >
-                        <FormattedMessage id="privacy_termsLink" />
-                    </Link>
-                </Grid> */}
-            </Grid>
+            </LinkContainer>
             <Dialog
                 aria-labelledby="policy-external-link"
                 fullScreen={useMediaQuery(theme.breakpoints.down("sm"))}
@@ -99,14 +107,14 @@ export default function PolicyLinks() {
                 scroll="paper"
                 TransitionComponent={Motion}
             >
-                <DialogAppBar handleClose={handleClose} titleID={title}/>
+                <DialogAppBar handleClose={handleClose} titleID={title} />
                 <DialogContent dividers>
                     <DialogContentText
                         id="scroll-content"
                         ref={descriptionElementRef}
                         tabIndex={-1}
                     >
-                        <PrivacyNotice/>
+                        <PrivacyPolicy />
                     </DialogContentText>
                 </DialogContent>
             </Dialog>
