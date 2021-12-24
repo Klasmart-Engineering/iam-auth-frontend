c5c3312587fabb4443cb0ced8548c0f79f106b31
Swapnali Bhansali <swapnali.bhansali@kidsloop.live>
2021-12-24 11:52:06 +0000

    change message

--- cypress/integration/common/globalBefore.js
+++ cypress/integration/common/globalBefore.js
@@ -2,7 +2,7 @@
 before(() => {
   cy.viewport('iphone-6')
   cy.log(
-    "This will run once before all tests, you can use this to for example start up your server, if that's your thing"
+    "Starting IAM regression"
   );
 });
 
