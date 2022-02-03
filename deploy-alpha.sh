f2ce760a1693b3fc44cf9dce42f4eb91b6349efb
Mateen Hussain <mateen.hussain@kidsloop.live>
2022-02-03 10:11:12 +0000

    Merged in feature-azureb2c-alpha (pull request #162)
    Feature azureb2c alpha
    
    * enable B2C for Alpha
    
    * update deployment script
    
    
    Approved-by: Hamza Mihai Daniel
    Approved-by: MihaiAlexandru.Rosu
    Approved-by: Max Nicholson

--- deploy-alpha.sh
+++ deploy-alpha.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
-git submodule update --init --recursive --force
 
 npm ci --no-progress
 mv ./.env ./.env.temp
 cp deploy/config/internal/.env.alpha ./.env
 npm run build
 mv ./.env.temp ./.env
-
-# install deps for deps
-pushd src/pages/account/kidsloop-pass-frontend/client
-npm ci --no-progress
-npm run build:kl-alpha
-popd
-
-# bundle deps build and main build
-mv src/pages/account/kidsloop-pass-frontend/client/dist ./dist/account
 
 aws s3 sync dist s3://kidsloop-alpha-auth-rested-mackerel/alpha/latest
 aws cloudfront create-invalidation --paths "/*" --distribution-id ELJUEQIM62ZJZ #auth.alpha.kidsloop.net
