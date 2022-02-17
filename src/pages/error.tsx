4ae39d3334679afa93a93960b3c1e1126c82ac75
Rosu Mihai <MihaiAlexandru.Rosu@endava.com>
2022-02-17 17:36:59 +0200

    remove unused references

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,5 +1,4 @@
 import GenericError from "@/components/GenericError";
-import GenericErrorProps from "@/components/interfaces/GenericErrorProps";
 import * as React from "react";
 
 export function Error () {
