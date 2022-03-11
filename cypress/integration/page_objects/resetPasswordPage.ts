4500e5bc936ae907aa3be2da4fbf86b676a4b7f8
Denis Ursan <denis.ursan@endava.com>
2022-03-11 10:05:04 +0200

    changed wait

--- cypress/integration/page_objects/resetPasswordPage.ts
+++ cypress/integration/page_objects/resetPasswordPage.ts
@@ -85,16 +85,9 @@ class ResetPasswordPage {
     }
 
     clickOnCreateOrContinueButton () {
-        cy.wait(1000).then(() => {
-            cy.log(`waited for 1 seconds`);
-        });
-        cy.get(this.createOrContinueButton).should('exist').click({
+        cy.get(this.createOrContinueButton,{timeout:7000}).should('exist').click({
             force: true,
         });
-        cy.wait(1000).then(() => {
-            cy.log(`waited for 1 seconds`);
-        });
-
     }
 
     clickOnSendNewCodeButtonPhoneNumber () {
