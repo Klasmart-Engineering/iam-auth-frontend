387db7a8f63ce992d4760e400d47d3b9ac15d5b9
Max Nicholson <max.nicholson@kidsloop.live>
2022-03-16 12:03:20 +0000

    fix: MCB initial login
    Initiate MCB login from auth-frontend, rather than from MCB
    They will navigate to `auth.kidsloop.in/signin?idp=mcb`
    
    ATH-240

--- src/hooks/azureB2C/useRedirectRequest.tsx
+++ src/hooks/azureB2C/useRedirectRequest.tsx
@@ -12,26 +12,44 @@ import { mapKidsloopLocaleToAzureB2CLocale } from "@/utils/azureB2C/locale";
 import type { RedirectRequest } from "@azure/msal-browser";
 import { useMemo } from "react";
 
-const buildRedirectRequest = ({ platform, locale }: {platform: Platform; locale: Locale}): RedirectRequest => {
+const buildRedirectRequest = ({
+    platform, locale, idp,
+}: {platform: Platform; locale: Locale; idp?: string}): RedirectRequest => {
+    const extraQueryParameters: RedirectRequest["extraQueryParameters"] = {
+        ui_locales: mapKidsloopLocaleToAzureB2CLocale(locale),
+    };
+
+    if (idp) {
+        extraQueryParameters[`idps`] = idp;
+    }
+
     return {
         ...loginRequest,
+        domainHint: idp,
         state: encodeState({
             platform,
         }),
-        extraQueryParameters: {
-            ui_locales: mapKidsloopLocaleToAzureB2CLocale(locale),
-        },
+        extraQueryParameters,
     };
 };
 
-const useRedirectRequest = (): RedirectRequest => {
+interface Props {
+    idp?: string;
+}
+
+const useRedirectRequest = ({ idp }: Props = {}): RedirectRequest => {
     const [ locale ] = useLocale();
     const platform = usePlatform();
 
     return useMemo(() => buildRedirectRequest({
         platform,
         locale,
-    }), [ locale, platform ]);
+        idp,
+    }), [
+        locale,
+        platform,
+        idp,
+    ]);
 };
 
 export default useRedirectRequest;
