1a391a832ef0a69e06f6b89fcbf44d7db3e9826a
Max Nicholson <max.nicholson@kidsloop.live>
2022-03-16 12:03:25 +0000

    chore: del AuthorizationCodeAuthenticationTemplate
    Requires a rethink to fix ATH-241, which is out of scope of this PR

--- src/components/azureB2C/index.tsx
+++ src/components/azureB2C/index.tsx
@@ -1,3 +1,2 @@
 export { default as AuthenticationFailed } from "./AuthenticationFailed";
-export { default as AuthorizationCodeAuthenticationTemplate } from "./AuthorizationCodeAuthenticationTemplate";
 export { default as AzureB2CProvider } from "./Provider";
